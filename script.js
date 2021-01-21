61bc8a0ae273d746a0ef6a04f7253f21b5b1245b
Diógenes Souza <diogenes.edusouza@gmail.com>
2021-01-20 22:14:42 -0300

    code Update
    20200120

--- script.js
+++ script.js
@@ -31,33 +31,55 @@ class AudioController {
     }
 }
 
-class mixOrMatch(totalTime, cards) {
-    constructor() {
+
+class MixOrMatch {
+    constructor(totalTime, cards) {
         this.cardsArray = cards;
         this.totalTime = totalTime;
-        thhis.timeRemaining = TotalTime;
+        this.timeRemaining = totalTime;
+        this.timer = document.getElementsByClassName('time-remaining');
+        this.ticker = document.getElementsByClassName('flips');
+        this.audioController = new AudioController();
     }
     startGame() {
         this.cardToCheck = null;
+        this.totalClicks = 0;
+        this.timeRemaining = this.totalTime;
+        this.matchedCards = [];
+        this.busy = true;
+    }
+    flipCard(card) {
+        if(this.canFlipCard(card)) {
+            this.audioController.flip();
+            this.totalClicks++;
+            this.ticker.innerText = this.totalClicks;
+        }
+    }
+    canFlipCard(card) {
+        return true;
+        //return !this.busy && !this.matchedCards.includes(cards) && card !== this.cardToCheck;
     }
 }
+
 
 function ready() {
     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
     let cards = Array.from(document.getElementsByClassName('card'));
+    let game = new MixOrMatch(100, cards);
 
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
             overlay.classList.remove('visible');
-            // game.startGame();
+            game.startGame();
         });
     });
-    cards.forEach( card => {
+    cards.forEach(card => {
         card.addEventListener('click', () => {
-            // game.flipCard(card);
+            game.flipCard(card);
         });
     });
 }
+
 
 if(document.readyState === 'loading') {
     document.addEventListener('DOMContentloaded', ready());
